02b87b3b1fd25f88f48a259c8a70c9a5fd01dbb3
schepieb <98114427+schepieb@users.noreply.github.com>
2022-05-12 18:45:04 +0200

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BOARD)
+# set pin 5 to input, and enable the internal pull-up resistor
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+oldButtonState1 = True
+while True:
+    buttonState1 = GPIO.input(5)
+
+    if buttonState1 != oldButtonState1 and buttonState1 == False :
+
+        subprocess.call("init 0", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    oldButtonState1 = buttonState1
+
+    time.sleep(.1)
